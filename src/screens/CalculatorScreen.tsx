b1aeba0933c9da8e634c9b15cab41a0a1653b9e6
juliflorezg <forezjuli08@hotmail.com>
2022-12-09 10:41:21 -0500

    add BotonCalc component to CalculatorScreen

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {BotonCalc} from '../components/BotonCalc';
 import {styles} from '../theme/appTheme';
 export const CalculatorScreen = () => {
   return (
@@ -7,11 +8,12 @@ export const CalculatorScreen = () => {
       <Text style={styles.resultadoPequeno}>1,500.00</Text>
       <Text style={styles.resultado}>1,500.00</Text>
 
-      <View>
+      <View style={styles.fila}>
         {/* Boton */}
-        <View style={styles.boton}>
-          <Text style={styles.botonTexto}>1</Text>
-        </View>
+        <BotonCalc text="C" backgroundColor="#9b9b9b" />
+        <BotonCalc text="+/-" backgroundColor="#9b9b9b" />
+        <BotonCalc text="del" backgroundColor="#9b9b9b" />
+        <BotonCalc text="/" backgroundColor="#ff9427" />
       </View>
     </View>
   );
